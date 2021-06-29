e0240c246fc0d082987dc8ed8ec03bd736b4fa5f
liyi2020 <liyi2020@boco.com.cn>
2021-06-29 11:13:14 +0800

    Initial commit

---
+++ io/src/main/java/file/CreateFile.java
@@ -0,0 +1,20 @@
+package file;
+
+import java.io.File;
+
+/**
+ * @author liyi
+ * @create 2021 -06 -29 -11:10
+ */
+public class CreateFile {  // 创建 File 的方式
+    public static void main(String[] args) {
+        // 方式一： \\
+        File file1 = new File("E:\\text.txt");
+        System.out.println("---------- 1 ---------"+file1);
+        // 方式二：
+
+    }
+
+
+
+}
